4d05da23da3e6cfa0e4cd2dc14dee5254aa4c6ab
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2024-07-08 14:34:44 +0200

    added nice animation as page waits for user selection

--- js/flower_type.js
+++ js/flower_type.js
@@ -2,29 +2,28 @@ function showflower(type) {
     let element = document.getElementById(type);
     
     switch (element.value) {
-        case "RedRose":
-            console.log(element.value);
+        case "red-roses":
             document.getElementById("chosen_flower").src='../image/bouquet.png';
             break;
 
         case "white-roses":
-            document.getElementById("splash_image").src='../image/White-Roses-Bouquet.png';
+            document.getElementById("chosen_flower").src='../image/White-Roses-Bouquet.png';
             break;
 
-        case "flower-baskets":
-            document.getElementById("splash_image").src='../image/flower-basket.png';
+        case "flower-basket":
+            document.getElementById("chosen_flower").src='../image/flower-basket.png';
             break;
 
         case "corsage":
-            document.getElementById("splash_image").src='../image/blush-wrist-corsage.png';
+            document.getElementById("chosen_flower").src='../image/blush-wrist-corsage.png';
             break;
 
         case "flower-wreath":
-            document.getElementById("splash_image").src='../image/flower-wreath.png';
+            document.getElementById("chosen_flower").src='../image/flower-wreath.png';
             break;
     
         default:
-            document.getElementById("splash_image").src='../image/beautiful-natural-red-rose-flowers-bouquet.png';
+            document.getElementById("chosen_flower").src='../image/loading.gif';
             break;
     }
 }
